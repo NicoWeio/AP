ae395193b764635b372c5ebc25881362fe429598
Nicolai Weitkemper <nico.weio@gmail.com>
2020-11-10 23:38:59 +0100

    (WIP)

--- D206_Waermepumpe/main.py
+++ D206_Waermepumpe/main.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties import ufloat
 import uncertainties.unumpy as unp
 import scipy.optimize as sp_optimize
 import scipy.stats as sp_stats
+#TODO
+import sys
 
 t, T1, p1, T2, p2, N = np.genfromtxt('Daten.dat', unpack=True)
 
@@ -15,6 +18,11 @@ T2 += 273.15
 # Da die mit "*" versehenen Werte aus "Daten und Hinweise.pdf" mit denen ohne "*" aus "Daten.dat" übereinstimmen, liegt es tatsächlich an uns, 1 bar zu addieren.
 p1 += 1
 p2 += 1
+
+# „Die Messunsicherheit des digitalen Thermometers kann als ∆T = 0.1 °C angenommen werden.“
+# (0.1 °C = 0.1 K)
+T1 = unp.uarray(T1, np.full(len(T1), 0.1))
+T2 = unp.uarray(T2, np.full(len(T2), 0.1))
 
 # x-Werte für die Approximation
 t_linspace = np.linspace(t[0], t[-1])
@@ -31,15 +39,16 @@ def fit_fn(x, params):
     A, B, C = params
     return A*x**2 + B*x + C
 
-# Für Aufgabe 5c:
+# --- Für Aufgabe 5c:
 def fit_fn_derivate(x, params):
     A, B, C = params
     return 2*A*x + B
+# ---
 
 def polyfit(T):
     params, covariance_matrix = np.polyfit(t, T, deg=2, cov=True)
     errors = np.sqrt(np.diag(covariance_matrix))
-    return params
+    return params, errors
 
 # def fit_fn_2(t, A, B):
 #     alpha = 1
@@ -53,48 +62,64 @@ def polyfit(T):
 #     popt, pcov = sp_optimize.curve_fit(fit_fn_3, t, T)
 #     return popt
 
-fit_params_T1 = polyfit(T1)
-fit_params_T2 = polyfit(T2)
+fit_params_T1, fit_errors_T1 = polyfit(unp.nominal_values(T1))
+fit_params_T2, fit_errors_T2 = polyfit(unp.nominal_values(T2))
 
-print("fit_params:", fit_params_T1, fit_params_T2)
+print("T1:")
+for param, error, i in zip(fit_params_T1, fit_errors_T1, ["A","B","C"]):
+    print(f"{i} = {param:.5} ± {error:.5}")
+print("T2:")
+for param, error, i in zip(fit_params_T2, fit_errors_T2, ["A","B","C"]):
+    print(f"{i} = {param:.5} ± {error:.5}")
+
 
 plt.plot(t_linspace, fit_fn(t_linspace, fit_params_T1), label=r"Approximation $T_1$")
-# plt.plot(t_linspace, fit_fn_2(t_linspace, *curve_fit(T1)), label=r"Approximation $T_1$")
-# plt.plot(t_linspace, fit_fn_3(t_linspace, *curve_fit(T1)), label=r"Approximation $T_1$")
-plt.errorbar(t, T1,  fmt='x', label=r'$T_1$')
-#plt.errorbar(t, p1, yerr = p1 * 0.68, fmt='o', label='p1')
+plt.errorbar(t, unp.nominal_values(T1), fmt='x', yerr=unp.std_devs(T1), label=r'$T_1$')
 
 plt.plot(t_linspace, fit_fn(t_linspace, fit_params_T2), label=r"Approximation $T_2$")
-# plt.plot(t_linspace, fit_fn_2(t_linspace, *curve_fit(T2)), label=r"Approximation $T_2$")
-# plt.plot(t_linspace, fit_fn_3(t_linspace, *curve_fit(T2)), label=r"Approximation $T_2$")
-plt.errorbar(t, T2,  fmt='x', label=r'$T_2$')
-#plt.errorbar(t, p2, yerr = p2 * 0.68, fmt='o', label='p2')
+plt.errorbar(t, unp.nominal_values(T2), fmt='x', yerr=unp.std_devs(T2), label=r'$T_2$')
 
-#plt.errorbar(t, N, fmt='x', label='N')
-
-# TODO: Einheiten passen nicht…
 plt.figure(1)
-plt.xlabel(r'$t / s$')
+plt.xlabel(r'$t \; / \; s$')
 plt.xlim(t.min(), t.max())
-plt.ylabel(r'$T / K$')
+plt.ylabel(r'$T \; / \; K$')
 plt.legend()
 plt.tight_layout()
 # plt.savefig('build/wärmepumpe_plot.pdf')
-# plt.show()
 
 
 ## Aufgabe 5c:
-# TODO Fehlerrechnung!
+
+# DERIV_INDICES = [7,14,21,28]
+DERIV_INDICES = [8,16,24,32] # → Tahirbanane
+
+def fit_fn_derivate_error(x, paramErrors):
+    A_err, B_err, C_err = paramErrors
+    return np.sqrt((2*x*A_err)**2 + (B_err)**2)
+
+# coolParams_T1 = [ufloat(p[0], p[1]) for p in zip(fit_params_T1, fit_errors_T1)]
+# print(coolParams_T1)
 
 # 7-14-21-28 → equal spacing between 0-35
-derivs_T1 = [fit_fn_derivate(x, fit_params_T1) for x in [t[7], t[14], t[21], t[28]]]
-derivs_T2 = [fit_fn_derivate(x, fit_params_T2) for x in [t[7], t[14], t[21], t[28]]]
-print("5c) derivs", derivs_T1, derivs_T2)
+# derivs_T1 =        [fit_fn_derivate(x, fit_params_T1)        for x in [t[7], t[14], t[21], t[28]]]
+# derivs_T1_errors = [fit_fn_derivate_error(x, fit_errors_T1)  for x in [t[7], t[14], t[21], t[28]]]
+# derivs_T2 =        [fit_fn_derivate(x, fit_params_T2)        for x in [t[7], t[14], t[21], t[28]]]
+# derivs_T2_errors = [fit_fn_derivate_error(x, fit_errors_T2)  for x in [t[7], t[14], t[21], t[28]]]
+# print("5c) derivs T1", derivs_T1, derivs_T1_errors)
+# print("5c) derivs T2", derivs_T2, derivs_T2_errors)
+derivs_T1 = [ufloat(fit_fn_derivate(t[i], fit_params_T1), fit_fn_derivate_error(t[i], fit_errors_T1)) for i in DERIV_INDICES]
+derivs_T2 = [ufloat(fit_fn_derivate(t[i], fit_params_T2), fit_fn_derivate_error(t[i], fit_errors_T2)) for i in DERIV_INDICES]
+print("5c) derivs T1", derivs_T1)
+print("5c) derivs T2", derivs_T2)
+
+# sys.exit()
 
 ## Aufgabe 5d:
-gueteziffern_ideal = T1 / (T1 - T2)
-print("gueteziffern_ideal", gueteziffern_ideal)
-# ✓ stimmt überein mit Tahirbanane
+
+for i in DERIV_INDICES:
+    gueteziffer_ideal = T1[i] / (T1[i] - T2[i])
+    print(f"Güteziffer für Minute {i} / t={t[i]:.0f}: {gueteziffer_ideal}")
+# ✓ Werte (für andere t!) stimmen überein mit Tahirbanane, unser Fehler ist aber größer
 
 mkck = 750 #J/K
 m1cw = 13293 # → Mampfzwerg
