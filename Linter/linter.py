0fbed3ab66ec333fb7288e28c420715355398649
Nicolai Weitkemper <nico.weio@gmail.com>
2020-12-29 22:34:08 +0100

    Linter in CI-Workflow integriert 🎉

--- Linter/linter.py
+++ Linter/linter.py
@@ -2,6 +2,7 @@
 
 import argparse
 import itertools
+import json
 from pathlib import Path
 import re
 from rules import rules
@@ -21,7 +22,6 @@ def line_number(newline_indices, index):
     return None
 
 def lint(lines):
-
     results = []
 
     for rule in rules:
@@ -30,6 +30,7 @@ def lint(lines):
             for match in re.finditer(rule['regex'], line, re.IGNORECASE):
                 results.append({
                 'message': rule['message'],
+                'importance': rule['importance'],
                 'line_index': line_index,
                 # 'char_index_in_line': match.start(),
                 })
@@ -37,53 +38,121 @@ def lint(lines):
     return results
 
 def group_warnings(lst):
-    # keyfunc = lambda d: next(iter(d.values()))
     keyfunc = lambda d: d['message']
     return itertools.groupby(sorted(lst, key=keyfunc), key=keyfunc)
-    # return {k: [x for d in g for x in d] for k, g in itertools.groupby(sorted(lst, key=keyfunc), key=keyfunc)}
+
+def group_lines(lst):
+    # lst has to be filtered by message for correct output!
+    lst = list(lst)
+    startLine = None
+    result = []
+    for i, item in enumerate(lst):
+        currentLine = item['line_index']
+        nextLine = lst[i+1]['line_index'] if (i < len(lst) - 1) else None
+        if (currentLine + 1 == nextLine):
+            # print("MULTI", currentLine)
+            if not startLine:
+                startLine = currentLine
+        elif startLine:
+            # print("MULTI_END", currentLine)
+            item['line_index'] = (startLine, currentLine)
+            result.append(item)
+            startLine = None
+        else:
+            # print("SINGLE", currentLine, nextLine)
+            item['line_index'] = (currentLine, currentLine)
+            result.append(item)
+    return result
+
+def output_json():
+    allResults = []
+    for path in files_to_lint():
+        with open(path, 'r') as f:
+            text = f.read()
+            lines = text.split('\n')
+
+            def get_annotation_level(importance):
+                # if importance > 8:
+                #     return 'failure'
+                if importance > 4:
+                    return 'warning'
+                return 'notice'
+
+            def resultify(w):
+                return {
+                    'file': str(path),
+                    'start_line': w['line_index'][0] + 1, # index shift from 0 to 1
+                    'end_line': w['line_index'][1] + 1, # index shift from 0 to 1
+                    # 'title': 'Lint',
+                    'message': w['message'],
+                    'annotation_level': get_annotation_level(w['importance'])
+                }
+
+            # [warning]
+            raw_result = lint(lines)
+            # [(message, [warning])]
+            grouped_result = group_warnings(raw_result)
+            # [warning]
+            line_grouped_result = []
+            for message, items in grouped_result:
+                line_grouped_result += group_lines(items)
+
+            results = [resultify(warning) for warning in line_grouped_result]
+            allResults += results
+
+    allResultsJson = json.dumps(allResults, indent=4)
+    print(allResultsJson)
+
+def output_human_readable():
+    summary_chars = 0
+    summary_lines = 0
+    summary_lines_with_error = 0
+    
+    for path in files_to_lint():
+        with open(path, 'r') as f:
+            text = f.read()
+            lines = text.split('\n')
+
+            summary_chars += len(text)
+            summary_lines += len(lines)
+
+            results = lint(lines)
+
+            if args.ignore_comments:
+                results = [r for r in results if not lines[r['line_index']].strip().startswith('%')]
+
+            if results:
+                total_unique_lines = len(set([r['line_index'] for r in results]))
+                summary_lines_with_error += total_unique_lines
+                print(f"{Colors.PATH}*** {path}: {len(results)} Warnung(en) in {total_unique_lines} Zeile(n){Colors.ENDC}")
+                # print(colored(f"*** {path}: {len(results)} warnings", 'yellow'))
+                grouped = group_warnings(results)
+                for message, items in grouped:
+                    print(Colors.MESSAGE + message + Colors.ENDC)
+                    unique_lines = sorted(set([i['line_index'] for i in items]))
+                    if args.show_lines:
+                        for i_l, l in enumerate(unique_lines):
+                            # if not first line and previous line index isn't one before current line index
+                            if (i_l != 0 and unique_lines[i_l-1] != l-1):
+                                print('–')
+                            # lines are zero-indexed, but the user expects them to be one-indexed
+                            print(l+1, Colors.CONTENT + lines[l].strip() + Colors.ENDC)
+                    else:
+                        print("Z. " + ','.join([str(l+1) for l in unique_lines]))
+
+    print('\n')
+    print(f"Insgesamt {summary_lines} Zeile(n), davon {summary_lines_with_error} mit Warnungen")
+    print(f"→ {(100*summary_lines_with_error/summary_lines):.2f}%")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', "--ignore-comments", help="ignore lines starting with a percent sign (including preceding whitespace)", action="store_true")
 parser.add_argument('-l', "--show-lines", help="show lines producing warnings", action="store_true")
+parser.add_argument("--json", help="export as JSON", action="store_true")
 args = parser.parse_args()
 
-summary_chars = 0
-summary_lines = 0
-summary_lines_with_error = 0
 
-for path in files_to_lint():
-    with open(path, 'r') as f:
-        text = f.read()
-        lines = text.split('\n')
-
-        summary_chars += len(text)
-        summary_lines += len(lines)
-
-        results = lint(lines)
-
-        if args.ignore_comments:
-            results = [r for r in results if not lines[r['line_index']].strip().startswith('%')]
-
-        if results:
-            total_unique_lines = len(set([r['line_index'] for r in results]))
-            summary_lines_with_error += total_unique_lines
-            print(f"{Colors.PATH}*** {path}: {len(results)} Warnung(en) in {total_unique_lines} Zeile(n){Colors.ENDC}")
-            # print(colored(f"*** {path}: {len(results)} warnings", 'yellow'))
-            grouped = group_warnings(results)
-            for message, items in grouped:
-                print(Colors.MESSAGE + message + Colors.ENDC)
-                unique_lines = sorted(set([i['line_index'] for i in items]))
-                if args.show_lines:
-                    for i_l, l in enumerate(unique_lines):
-                        # if not first line and previous line index isn't one before current line index
-                        if (i_l != 0 and unique_lines[i_l-1] != l-1):
-                            print('–')
-                        # lines are zero-indexed, but the user expects them to be one-indexed
-                        print(l+1, Colors.CONTENT + lines[l].strip() + Colors.ENDC)
-                else:
-                    print("Z. " + ','.join([str(l+1) for l in unique_lines]))
-
-
-print('\n')
-print(f"Insgesamt {summary_lines} Zeile(n), davon {summary_lines_with_error} mit Warnungen")
-print(f"→ {(100*summary_lines_with_error/summary_lines):.2f}%")
+if args.json:
+    output_json()
+else:
+    output_human_readable()
--- Linter/rules.py
+++ Linter/rules.py
@@ -2,31 +2,39 @@ rules = [
 {
 'regex': r'\b(wir|uns[er]*|ich)\b',
 'message': '"wir/uns/…" vermeiden',
+'importance': 10,
 }, {
 'regex': r'\bman\b',
 'message': '"man" vermeiden',
+'importance': 10,
 }, {
 'regex': r'(?<!\\symup{)\\Delta',
 'message': 'Delta wird aufrecht geschrieben → symup!',
+'importance': 5,
 }, {
 # matcht auch "11.11.2020"
 'regex': r'\d{5,}',
 'message': 'lange Zahlen vermeiden',
+'importance': 6,
 }, {
 # 'regex': r'0\.00',
 'regex': r'0{3,}',
 'message': 'Zahlen mit vielen Nullen vermeiden → wissenschaftliche Schreibweise?',
+'importance': 4,
 }, {
 'regex': r'\+\/-|-\/\+',
 'message': '+/- (bzw. -/+) in Textform → \\pm (bzw. \\mp) verwenden',
+'importance': 5,
 },
 {
 # https://de.wiktionary.org/wiki/Modul#Substantiv,_m
 'regex': r'Elastizitätsmodule',
-'message': 'DER E-Modul, nicht DAS E-Modul',
+'message': 'Die E-Moduln (m.), nicht die E-Module (n.)',
+'importance': 8,
 }, {
 #TODO: sollte *immer* Kommentar-Zeilen berücksichtigen
 'regex': r'TODO',
 'message': 'TODO-Notiz',
+'importance': 7,
 },
 ]
