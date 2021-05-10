7634dc1e94e16963515de6eb7c19f875486820d8
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2021-05-10 15:51:59 +0200

    D602: Abgabe

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import numpy as np
 import pint
 import scipy as sp
 import scipy.stats
-from uncertainties import ufloat
+from uncertainties import ufloat, UFloat
 
 def linregress(x, y):
     r = sp.stats.linregress(x.m, y.m)
@@ -28,10 +28,22 @@ def fmt_abs_err(own, reference, precise=False):
     return f'{(own_abs_err).n:.2f}'
 
 
-def fmt_rel_err_percent(o, r, precise=False):
+def fmt_rel_err_percent(o, r, precise=False, show_uncertainty=True):
     o_rel_err = ((o - r)/r).to('dimensionless').m
-    return f'{(o_rel_err * 100).n:.2f} %'
+    if isinstance(o_rel_err, UFloat) and not show_uncertainty:
+        o_rel_err = o_rel_err.n
+    return f'{o_rel_err:.2%}'
 
 
 def fmt_err(o, r, precise=False):
     return f'{fmt_rel_err_percent(o, r, precise)} | {fmt_abs_err(o, r, precise)}'
+
+def fmt_compare_to_ref(o, r, name=None, unit=None):
+    my_o = o.to(unit) if unit else o
+    my_r = r.to(unit) if unit else r
+    my_name = f'{name}:\n' if name else ''
+    return my_name + (
+        f'- ist: {my_o:.2f}\n'
+        f'- soll: {my_r:.2f}\n'
+        f'- rel. Abweichung: {fmt_rel_err_percent(o, r)}'
+    )
